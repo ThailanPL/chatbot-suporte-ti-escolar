02e4239dbd6f9bcf798a06b7c5629bd27e1255ee
Thailan Pereira Lima <lthailan99@gmail.com>
2026-08-24 17:25:24 -0300

    Delete .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++
@@ -1,7 +0,0 @@
-/**
- * Impede que o Puppeteer tente baixar uma cópia própria do Chrome.
- * O bot usa o Chrome, Edge ou Brave já instalado no computador.
- */
-module.exports = {
-  skipDownload: true,
-};
